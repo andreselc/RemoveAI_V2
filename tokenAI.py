ea1ca6c97a089bb497f2404d32274fa0986bf4c8
Andrés <95057542+andreselc@users.noreply.github.com>
2024-05-22 15:53:44 -0400

    AI Model Implemented

--- tokenAI.py
+++ tokenAI.py
@@ -1,5 +1,17 @@
 from getpass import getpass
 import os
+import replicate
+from dotenv import load_dotenv
 
-REPLICATE_API_TOKEN = getpass()
-os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
+load_dotenv()
+REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
+
+input = {
+  "video": open("./thatsMyOpinion.mp4", "rb")
+}
+
+output = replicate.run(
+  "nateraw/video-background-remover:ac5c138171b04413a69222c304f67c135e259d46089fc70ef12da685b3c604aa",
+  input=input
+)
+print(output)
